4ec2215f66855616da9aaf9057778a1e71a5b208
louayahmad <58867391+louayahmad@users.noreply.github.com>
2020-09-28 10:21:59 -0400

    Update Lockable.java

--- Lockable.java
+++ Lockable.java
@@ -2,23 +2,12 @@ package question4;
 
 public interface Lockable
 {
-   //-----------------------------------------------------------------
-   //  Sets the object's key.
-   //-----------------------------------------------------------------
+
    public void setKey(int key);
 
-   //-----------------------------------------------------------------
-   //  Locks the object if the proper key is provided.
-   //-----------------------------------------------------------------
    public void lock(int key);
-
-   //-----------------------------------------------------------------
-   //  Unlocks the object if the proper key is provided.
-   //-----------------------------------------------------------------
+   
    public void unlock(int key);
 
-   //-----------------------------------------------------------------
-   //  Returns true it the object is locked, else returns false.
-   //-----------------------------------------------------------------
    public boolean locked();
 }
